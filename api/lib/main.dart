a3e9d9c1fe24d1134409e32941daf46bf2d87025
shen0V0 <112296959+shen0V0@users.noreply.github.com>
2024-11-13 17:56:33 -0500

    git

---
+++ api/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'screens/quiz_screen.dart';
+void main() {
+runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+title: 'Trivia Quiz App',
+theme: ThemeData(primarySwatch: Colors.blue),
+home: QuizScreen(),
+);
+}
+}
